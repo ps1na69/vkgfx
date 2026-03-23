f6aec18806f6fcfe0b4a82aca014c1ea3595b154
Psina69 <ktoto14102010@gmail.com>
2026-03-23 22:03:19 +0200

    gitignore fix

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programming/vkgfx/tests
-# Build directory: D:/Programming/vkgfx/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/Programming/vkgfx/build/tests/vkgfx_tests-b12d07c_include.cmake")
-subdirs("../_deps/catch2-build")
